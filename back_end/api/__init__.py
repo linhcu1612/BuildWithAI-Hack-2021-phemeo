27f325c6818a0e0b2a572843f8a75534bb9aad1f
Andrews <andrews.trinh@gmail.com>
2021-11-02 10:39:55 +1100

    remove fail dependencies

--- back_end/api/__init__.py
+++ back_end/api/__init__.py
@@ -11,9 +11,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/sunmedical.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
--- back_end/main.py
+++ back_end/main.py
@@ -1,8 +1,7 @@
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
-from api import app,db
-from api import models
+from api import app
 from api.queries import query
 
 type_defs = load_schema_from_path("schema.graphql")
