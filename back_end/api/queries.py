415a86b7fe67f272b7beb6e5d76d45979e661d5d
Andrews <andrews.trinh@gmail.com>
2021-11-01 23:39:56 +1100

    api done

--- back_end/api/queries.py
+++ back_end/api/queries.py
@@ -1,17 +1,31 @@
-from .models import Patient
 from ariadne import ObjectType
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('api/data/clean_data.csv')
+data['name'] ='Anynomous'
+gpa_cols = [col for col in data.columns if col.endswith('SGPA')]
+data['gpa'] = data[gpa_cols].mean(axis=1)
+students_data = data.drop(columns=gpa_cols)
+students_data.columns = students_data.columns.str.lower()
+students_data = students_data.rename(columns={'label': 'mental_status'})
+students_data.columns
+students_data = students_data.to_dict('records')
+
 query = ObjectType("Query")
-@query.field("patients")
-def resolve_patients(obj, info):
+
+@query.field("Students")
+def resolve_students(*_,limit=None):
     try:
-        patients = [patient.to_dict() for patient in Patient.query.all()]
+        students = students_data[:limit]
         payload = {
             "success": True,
-            "patients": patients
+            "students": students
         }
     except Exception as error:
         payload = {
             "success": False,
-            "errors": [str(error)]
+            "errors": [str(error)],
+            "public_spaces": [],
         }
     return payload
