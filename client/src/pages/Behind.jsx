7d56d066435ceeded3edc5730ab111ed613620f5
Nhat Linh <nhatlinh161232@gmail.com>
2021-11-02 15:27:23 +1100

    fix small bug

--- client/src/pages/Behind.jsx
+++ client/src/pages/Behind.jsx
@@ -1,7 +1,6 @@
 /** @format */
 
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import behind_1 from "../assets/behind/behind_1.png";
@@ -14,16 +13,16 @@ export class Behind extends Component {
     return (
       <Carousel>
         <div>
-          <img src={behind_1} />
+          <img src={behind_1} alt='first slice' />
         </div>
         <div>
-          <img src={behind_2} />
+          <img src={behind_2} alt='second slice' />
         </div>
         <div>
-          <img src={behind_3} />
+          <img src={behind_3} alt='third slice' />
         </div>
         <div>
-          <img src={behind_4} />
+          <img src={behind_4} alt='last slice' />
         </div>
       </Carousel>
     );
